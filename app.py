ffce840bf6bf20c7858449cd592548f6adf31d17
Anzon11364 <123927655+Anzon11364@users.noreply.github.com>
2023-04-23 21:32:16 +0000

    made category Select page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import subprocess
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -12,9 +13,24 @@ def home():
 def reg():
   return render_template('register.html')
 
+@app.route('/TEST', methods=['POST'])
+def test():
+    email = request.form['createEmail']
+    password = request.form['createPass']
+    php_script = "templates/register.php"
+    php_output = subprocess.check_output(["php", php_script, email, password])
+    return render_template('TEST.html', email=email, password=password, php_output=php_output)
+
+
 @app.route("/register1")
 def p():
   return render_template('register.php')
+
+
+@app.route("/categorySelect")
+def category():
+  return render_template('categorySelect.html')
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- templates/register.php
+++ templates/register.php
@@ -1,17 +1,7 @@
 <?php
 include "connect.php";
-// Get the values from the form
-// $email = $_POST['createEmail'];
-// $password = $_POST['createPass'];
-// print_r($email);
-// print_r($password);
-// // preparing and executing statment
-// $sql = "INSERT into users(user_email, password)
-// values('$email','$password')";
-// $result = mysqli_query($conn, $sql);
-// if($result){
-// echo $firstname. " is registred succesfully!";
-// }
-// $conn->close();
-print_r($_POST);
+$email = $_POST['createEmail'];
+$password = $_POST['createPass'];
+echo "Email: " . $email . "<br>";
+echo "Password: " . $password;
 ?>
